6bf0d00bbc0f595bae3e95295c574836eaf345df
phucnguyen81 <phucknguyen@gmail.com>
2023-01-13 09:28:25 +0700

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import 'ag-grid-enterprise';
+import { ColDef, GridApi, GridReadyEvent, RowNode } from 'ag-grid-community';
+
+import { IOlympicData } from './interfaces';
 
 @Component({
   selector: 'my-app',
@@ -8,11 +12,71 @@ import 'ag-grid-enterprise';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  columnDefs = [{ field: 'make' }, { field: 'model' }, { field: 'price' }];
+  private gridApi!: GridApi<IOlympicData>;
 
-  rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
+  public columnDefs: ColDef[] = [
+    { field: 'country', rowGroup: true, hide: true },
+    { field: 'athlete', minWidth: 180 },
+    { field: 'age' },
+    { field: 'year' },
+    { field: 'date', minWidth: 150 },
+    { field: 'sport', minWidth: 150 },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+    { field: 'total' },
   ];
+  public defaultColDef: ColDef = {
+    flex: 1,
+    minWidth: 100,
+    sortable: true,
+    filter: true,
+  };
+  public autoGroupColumnDef: ColDef = {
+    minWidth: 200,
+  };
+  public groupDefaultExpanded = 1;
+  public rowData!: IOlympicData[];
+
+  constructor(private http: HttpClient) {}
+
+  onBtForEachNode() {
+    console.log('### api.forEachNode() ###');
+    this.gridApi.forEachNode(printNode);
+  }
+
+  onBtForEachNodeAfterFilter() {
+    console.log('### api.forEachNodeAfterFilter() ###');
+    this.gridApi.forEachNodeAfterFilter(printNode);
+  }
+
+  onBtForEachNodeAfterFilterAndSort() {
+    console.log('### api.forEachNodeAfterFilterAndSort() ###');
+    this.gridApi.forEachNodeAfterFilterAndSort(printNode);
+  }
+
+  onBtForEachLeafNode() {
+    console.log('### api.forEachLeafNode() ###');
+    this.gridApi.forEachLeafNode(printNode);
+  }
+
+  onGridReady(params: GridReadyEvent<IOlympicData>) {
+    this.gridApi = params.api;
+
+    this.http
+      .get<IOlympicData[]>(
+        'https://www.ag-grid.com/example-assets/olympic-winners.json'
+      )
+      .subscribe((data) => params.api!.setRowData(data.slice(0, 50)));
+  }
 }
+
+const printNode = (node: RowNode<IOlympicData>, index?: number) => {
+  if (node.group) {
+    console.log(index + ' -> group: ' + node.key);
+  } else {
+    console.log(
+      index + ' -> data: ' + node.data!.country + ', ' + node.data!.athlete
+    );
+  }
+};
